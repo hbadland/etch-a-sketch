7334711b4c809146477a67964d5844cbf5502623
hbadland <badlandh@icloud.com>
2024-08-01 19:51:10 +0100

    Added 16 x 16 grid

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const container = document.querySelector(".container");
+console.log("Javascript loaded")
 
-    for (let i=0; i < 16 * 16; i++) {
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.container');
+
+    for (let i = 0; i < 16 * 16; i++) {
         const square = document.createElement('div'); //creates new div element for each square
-        square.classList.add('grid-square') //adds grid squares to the div element to css
-        container.appendChild(square) //append the div element to the container from html
-    }
-})
+        square.classList.add('grid-square'); //adds grid squares to the div element to css
+        container.appendChild(square);
+    };
+});
+
