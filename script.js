60e1172d33e36de07ad87a09c8fc19b6442e7898
hbadland <badlandh@icloud.com>
2024-08-25 20:26:10 +0100

    Fixed input for grid size

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-console.log("Javascript loaded")
-
 document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector('.container');
+    const promptButton = document.querySelector("#promptButton");
 
-    for (let i = 0; i < 16 * 16; i++) {
+    let size = 16;
+    createGrid(size)
+
+    //need to create grid of specificed size
+    function createGrid(size) {
+        container.innerHTML = ''; //clear container
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div'); //creates new div element for each square
         square.classList.add('grid-square'); //adds grid squares to the div element to css
-        container.appendChild(square);
 
         square.onmouseover = function(event) {
             const bgColor = "gray";
@@ -14,6 +21,17 @@ document.addEventListener("DOMContentLoaded", function() {
         };
 
         container.appendChild(square);
-        console.log("Etch a sketch")
-    };
+    }
+}
+
+    promptButton.addEventListener("click", () => {
+        const input = prompt("How big do you want your grid?");
+        const newSize = parseInt(input, 10);
+        if (!isNaN(newSize) && newSize > 0) {
+                size = newSize;
+                createGrid(size);
+        } else {
+            alert("invalid input");
+        }
+    });
 });
